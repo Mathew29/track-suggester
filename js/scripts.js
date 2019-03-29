8fb39232dbad600759433317423a54ca531ae7cc
Mathew Akre <mwakre29@gmail.com>
2019-03-29 10:50:35 -0700

    add 1 if and 11 else if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("#track-suggest-form").submit(function(event) {
     event.preventDefault();
 
-    var company = $("input:radio[name=company]:checked").val();
     var frontEnd = $("input:radio[name=frontEnd]:checked").val();
     var webApplication = $("input:radio[name=webApplication]:checked").val();
     var fastPace = $("input:radio[name=fastPace]:checked").val();
@@ -10,18 +9,51 @@ $(document).ready(function() {
     var business = $("input:radio[name=business]:checked").val();
     var backEnd = $("input:radio[name=backEnd]:checked").val();
 
-    if (company === "enterprise" && frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "no" && highPerform === "no" && business === "no") {
+    if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "yes") {
+      $("#c").show();
       $("#java").show();
-    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "yes" && highPerform === "no" && business === "no") {
       $("#ruby").show();
-    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "yes" && fastPace === "yes" && high-perform === "no" && business === "no") {
-      $("#ruby").show();
-    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "yes" && high-perform === "no" && business === "no") {
       $("#php").show();
-    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "no" && high-perform === "yes" && business === "no") {
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "no"){
       $("#java").show();
-    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "no" && high-perform === "yes" && business === "no") {
+      $("#ruby").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "no" && business === "yes"){
+      $("#c").show();
       $("#java").show();
+      $("#ruby").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "no" && highPerform === "yes" && business === "yes"){
+      $("#c").show();
+      $("#java").show();
+      $("#ruby").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "yes" && highPerform === "yes" && business === "yes"){
+      $("#c").show();
+      $("#java").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "no" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "yes"){
+      $("#java").show();
+      $("#ruby").show();
+      $("#php").show();
+    } else if (frontEnd === "no" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "yes"){
+      $("#c").show();
+      $("#java").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "no" && business === "no"){
+      $("#ruby").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "yes" && fastPace === "no" && highPerform === "yes" && business === "no"){
+      $("#java").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "yes" && highPerform === "yes" && business === "no"){
+      $("#java").show();
+      $("#php").show();
+    } else if (frontEnd === "yes" && backEnd === "no" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "no"){
+      $("#java").show();
+      $("#ruby").show();
+      $("#php").show();
+    } else if (frontEnd === "no" && backEnd === "yes" && webApplication === "yes" && fastPace === "yes" && highPerform === "yes" && business === "no"){
+      $("#java").show();
+      $("#php").show();
     }
 
 
