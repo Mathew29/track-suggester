e210b8c5a6ae99325d953ac5a323ef7b60e79cf3
Mathew Akre <mwakre29@gmail.com>
2019-03-29 10:34:03 -0700

    remove first radio, remove if statment.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     event.preventDefault();
 
     var company = $("input:radio[name=company]:checked").val();
-    var frontend = $("input:radio[name=frontend]:checked").val();
+    var frontEnd = $("input:radio[name=frontEnd]:checked").val();
     var webApplication = $("input:radio[name=webApplication]:checked").val();
     var fastPace = $("input:radio[name=fastPace]:checked").val();
     var highPerform = $("input:radio[name=highPerform]:checked").val();
     var business = $("input:radio[name=business]:checked").val();
+    var backEnd = $("input:radio[name=backEnd]:checked").val();
 
-    if (frontEnd === "yes" && backEnd === "yes" && fastPace === "no") {
-      // console.log("hi")
+    if (company === "enterprise" && frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "no" && highPerform === "no" && business === "no") {
       $("#java").show();
-    } else if (frontEnd === "yes" && backEnd === "yes" && fastPace === "yes") {
+    } else if (frontEnd === "yes" && backEnd === "yes" && webApplication === "no" && fastPace === "yes" && highPerform === "no" && business === "no") {
       $("#ruby").show();
     } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "yes" && fastPace === "yes" && high-perform === "no" && business === "no") {
       $("#ruby").show();
@@ -22,6 +22,7 @@ $(document).ready(function() {
       $("#java").show();
     } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "no" && high-perform === "yes" && business === "no") {
       $("#java").show();
+    }
 
 
 
