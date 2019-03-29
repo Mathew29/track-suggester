7b57c9e9d4faea708b865916b371c6aafd19a21a
Mathew Akre <mwakre29@gmail.com>
2019-03-29 10:07:46 -0700

    fix syntax error for var names that had '-' in them

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,25 @@ $(document).ready(function() {
     event.preventDefault();
 
     var company = $("input:radio[name=company]:checked").val();
-    var front-end  = $("input:radio[name=front-end]:checked").val();
-    var web-application = $("input:radio[name=web-application]:checked").val();
-    var fast-pace = $("input:radio[name=fast-pace]:checked").val();
-    var high-perform = $("input:radio[name=high-perform]:checked").val();
+    var frontend = $("input:radio[name=frontend]:checked").val();
+    var webApplication = $("input:radio[name=webApplication]:checked").val();
+    var fastPace = $("input:radio[name=fastPace]:checked").val();
+    var highPerform = $("input:radio[name=highPerform]:checked").val();
     var business = $("input:radio[name=business]:checked").val();
+
+    if (frontEnd === "yes" && backEnd === "yes" && fastPace === "no") {
+      // console.log("hi")
+      $("#java").show();
+    } else if (frontEnd === "yes" && backEnd === "yes" && fastPace === "yes") {
+      $("#ruby").show();
+    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "yes" && fastPace === "yes" && high-perform === "no" && business === "no") {
+      $("#ruby").show();
+    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "yes" && high-perform === "no" && business === "no") {
+      $("#php").show();
+    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "no" && high-perform === "yes" && business === "no") {
+      $("#java").show();
+    } else if (company === "enterprise" && frontEnd === "yes" && backEnd === "no" && webApplication === "no" && fastPace === "no" && high-perform === "yes" && business === "no") {
+      $("#java").show();
 
 
 
